b456c505c6471fe0b4ab32011f1976e4abe0637e
zrd <2854716957@qq.com>
2025-03-21 16:09:09 +0800

    feat(redis): 完成Redis代码初始化

---
+++ src/main/java/com/zrd/springbootinit/constant/RedisConstant.java
@@ -0,0 +1,17 @@
+package com.zrd.springbootinit.constant;
+
+/**
+ * 通用常量
+ *
+ * @author <a href="https://github.com/shark688">shark688</a>
+  
+ */
+public interface RedisConstant {
+
+    /**
+     * 验证码Key
+     */
+    String PHONE_NUMBER_KEY = "user:phoneNumber:";
+
+    
+}
