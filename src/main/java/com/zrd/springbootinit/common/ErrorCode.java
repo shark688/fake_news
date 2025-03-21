55d1997db48e245f867a425512cf18642675a30a
zrd <2854716957@qq.com>
2025-03-21 16:08:16 +0800

    refactor(Errorcode): 修改错误码

--- src/main/java/com/zrd/springbootinit/common/ErrorCode.java
+++ src/main/java/com/zrd/springbootinit/common/ErrorCode.java
@@ -9,8 +9,8 @@ package com.zrd.springbootinit.common;
 public enum ErrorCode {
 
     SUCCESS(0, "ok"),
-    PARAMS_ERROR(40000, "请求参数错误"),
-    NOT_LOGIN_ERROR(40100, "未登录"),
+    PARAMS_ERROR(400, "请求参数错误"),
+    NOT_LOGIN_ERROR(401, "未登录"),
     NO_AUTH_ERROR(40101, "无权限"),
     NOT_FOUND_ERROR(40400, "请求数据不存在"),
     FORBIDDEN_ERROR(40300, "禁止访问"),
