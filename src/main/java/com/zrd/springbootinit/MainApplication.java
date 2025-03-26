303feebad2651f53078512fbfd8e99715f116068
zrd <2854716957@qq.com>
2025-03-26 20:25:07 +0800

    refactor(MainApp): 开启Redis功能

--- src/main/java/com/zrd/springbootinit/MainApplication.java
+++ src/main/java/com/zrd/springbootinit/MainApplication.java
@@ -13,8 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * @author <a href="https://github.com/shark688">shark688</a>
   
  */
-// todo 如需开启 Redis，须移除 exclude 中的内容
-@SpringBootApplication(exclude = {RedisAutoConfiguration.class})
+@SpringBootApplication()
 @MapperScan("com.zrd.springbootinit.mapper")
 @EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
