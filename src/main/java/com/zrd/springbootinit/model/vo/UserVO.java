a1070900b5e8aaa90bb3a0db4137e1dcb8811385
zrd <2854716957@qq.com>
2025-03-26 20:27:56 +0800

    refactor(userVO): 用户视图重构

--- src/main/java/com/zrd/springbootinit/model/vo/UserVO.java
+++ src/main/java/com/zrd/springbootinit/model/vo/UserVO.java
@@ -1,8 +1,9 @@
 package com.zrd.springbootinit.model.vo;
 
+import lombok.Data;
+
 import java.io.Serializable;
 import java.util.Date;
-import lombok.Data;
 
 /**
  * 用户视图（脱敏）
@@ -18,30 +19,22 @@ public class UserVO implements Serializable {
      */
     private Long id;
 
-    /**
-     * 用户昵称
-     */
-    private String userName;
-
-    /**
-     * 用户头像
-     */
-    private String userAvatar;
-
-    /**
-     * 用户简介
-     */
-    private String userProfile;
+    private String phoneNumber;
 
     /**
      * 用户角色：user/admin/ban
      */
-    private String userRole;
+    private Integer userRole;
 
     /**
      * 创建时间
      */
     private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
 
     private static final long serialVersionUID = 1L;
 }
